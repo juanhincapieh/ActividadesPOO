313694c513eefbdee37ec9dc951434105fd379a3
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-02-27 21:30:15 -0500

    Update ejerciciopn14.java

--- Actividad 1/ejerciciopn14.java
+++ Actividad 1/ejerciciopn14.java
@@ -5,7 +5,7 @@ public class ejerciciopn14 {
     public static void main(String[] args) {
         double numero, cuadrado, cubo;
 
-        numero = 2;
+        numero = 6;
         cuadrado = Math.pow(numero, 2);
         cubo = Math.pow(numero, 3);
         
