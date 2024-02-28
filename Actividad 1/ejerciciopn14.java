97fde0ca71edd8ff639b7ac1fd0f2b7ae35e14ee
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-02-27 21:21:21 -0500

    Create ejerciciopn14.java

---
+++ Actividad 1/ejerciciopn14.java
@@ -0,0 +1,16 @@
+package main.java.ejercicioclase;
+
+public class ejerciciopn14 {
+
+    public static void main(String[] args) {
+        double numero, cuadrado, cubo;
+
+        numero = 2;
+        cuadrado = Math.pow(numero, 2);
+        cubo = Math.pow(numero, 3);
+        
+        System.out.println("El numero es: " + numero);
+        System.out.println("El cuadrado es: " + cuadrado);
+        System.out.println("El cubo es: " + cubo);
+    }
+}
