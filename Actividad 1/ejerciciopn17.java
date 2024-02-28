dd578326d4201a92fc6f92700586a2d51bb3f59a
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-02-27 21:30:05 -0500

    Create ejerciciopn17.java

---
+++ Actividad 1/ejerciciopn17.java
@@ -0,0 +1,16 @@
+package main.java.ejercicioclase;
+
+public class ejerciciopn17 {
+
+    public static void main(String[] args) {
+        double radio, area, longitud;
+
+        radio = 5;
+        area = Math.PI * Math.pow(radio, 2);
+        longitud = 2 * Math.PI * radio;
+
+        System.out.println("El radio del circulo es: " + radio);
+        System.out.println("El area del circulo es: " + area);
+        System.out.println("La longitud de la circunferencia es: " + longitud);
+    }
+}
