cfcde0b7ca5d2da2101f945d30e644de5205d472
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-08-03 21:52:36 -0500

    Create Cap4Ejercicio23.java

---
+++ Actividad 3/Parte 1/poo/Cap4Ejercicio23/Cap4Ejercicio23.java
@@ -0,0 +1,11 @@
+package poo.cap4ejercicio23;
+
+public class Cap4Ejercicio23 {
+
+    public static void main(String[] args) {
+        
+        VentanaEjercicio23 ventana = new VentanaEjercicio23();
+        ventana.show(true);
+        
+    }
+}
