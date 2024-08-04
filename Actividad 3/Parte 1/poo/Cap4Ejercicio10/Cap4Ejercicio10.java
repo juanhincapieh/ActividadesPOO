fdc5ea748286a7df3ca4346de4c3af256a38e805
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-08-03 21:49:10 -0500

    Create Cap4Ejercicio10.java

---
+++ Actividad 3/Parte 1/poo/Cap4Ejercicio10/Cap4Ejercicio10.java
@@ -0,0 +1,11 @@
+package poo.cap4ejercicio10;
+
+public class Cap4Ejercicio10 {
+
+    public static void main(String[] args) {
+        
+        VentanaEjercicio10 ventana = new VentanaEjercicio10();
+        ventana.show(true);
+        
+    }
+}
