7e4f1003b8f27deb2f8c52eef3de37562b2ead4e
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-08-03 22:07:42 -0500

    Create PruebaFiguras.java

---
+++ Actividad 3/Parte 2/poo/figurasgeometricasfix/PruebaFiguras.java
@@ -0,0 +1,18 @@
+package poo.figurasgeometricasfix;
+
+public class PruebaFiguras {
+
+    /**
+    * Metodo main que crea un circulo, un rectangulo, un cuadrado y
+    * un triangulo rectangulo. Para cada uno de estas figuras geometricas,
+    * se calcula su area y perimetro.
+    */
+
+    public static void main(String args[]) {
+        
+        VentanaPruebaFiguras ventana = new VentanaPruebaFiguras();
+        ventana.show(true);
+        
+    }
+    
+}
