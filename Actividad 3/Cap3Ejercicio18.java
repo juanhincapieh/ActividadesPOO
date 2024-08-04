53f7639a90d10f0998751e01a88c4079288c5e17
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-08-03 21:35:26 -0500

    Create Cap3Ejercicio18.java

---
+++ Actividad 3/Cap3Ejercicio18.java
@@ -0,0 +1,11 @@
+package poo.cap3ejercicio18;
+
+public class Cap3Ejercicio18 {
+
+    public static void main(String[] args) {
+        
+        VentanaEjercicio18 ventana = new VentanaEjercicio18();
+        ventana.show(true);
+        
+    }
+}
