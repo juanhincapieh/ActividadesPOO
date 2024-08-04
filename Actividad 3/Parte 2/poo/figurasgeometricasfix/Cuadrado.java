cfaddb2ec3271b5b29c7f56e55cb9cdad3d269a5
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-08-03 22:08:05 -0500

    Add files via upload

---
+++ Actividad 3/Parte 2/poo/figurasgeometricasfix/Cuadrado.java
@@ -0,0 +1,39 @@
+
+package poo.figurasgeometricasfix;
+
+
+public class Cuadrado {
+
+    int lado; // Atributo que define el lado de un cuadrado
+
+    /**
+    * Constructor de la clase Cuadrado
+    * @param lado Parametro que define la longitud de la base de un
+    * cuadrado
+    */
+
+    public Cuadrado(int lado) {
+        this.lado = lado;
+    }
+
+    /**
+    * Metodo que calcula y devuelve el area de un cuadrado como el
+    * lado elevado al cuadrado
+    * @return Area de un Cuadrado
+    */
+    
+    double calcularArea() {
+        return lado*lado;
+    }
+
+    /**
+    * Metodo que calcula y devuelve el perimetro de un cuadrado como
+    * cuatro veces su lado
+    * @return Perimetro de un cuadrado
+    */
+
+    double calcularPerimetro() {
+        return (4*lado);
+    }
+}
+
---
+++ Actividad 3/Parte 2/poo/figurasgeometricasfix/Rectangulo.java
@@ -0,0 +1,39 @@
+package poo.figurasgeometricasfix;
+
+public class Rectangulo {
+
+    int base; // Atributo que define la base de un rectangulo
+    int altura; // Atributo que define la altura de un rectangulo
+
+    /**
+    * Constructor de la clase Rectangulo
+    * @param base Parametro que define la base de un rectangulo
+    * @param altura Parametro que define la altura de un rectangulo
+    */
+
+    Rectangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    /**
+    * Metodo que calcula y devuelve el area de un rectangulo como la
+    * multiplicacion de la base por la altura
+    * @return Area de un rectangulo
+    */
+
+    double calcularArea() {
+        return base * altura;
+    }
+
+    /**
+    * Metodo que calcula y devuelve el perimetro de un rectangulo
+    * como (2 * base) + (2 * altura)
+    * @return Perimetro de un rectangulo
+    */
+
+    double calcularPerimetro() {
+        return (2 * base) + (2 * altura);
+    }
+}
+
