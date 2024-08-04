e6ca61c2c9b9ab2e2d6301b0ab126afec1685e0a
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-08-03 21:47:15 -0500

    Create Cap4Ejercicio7Final.java

---
+++ Actividad 3/Parte 1/poo/Cap4Ejercicio7Final/Cap4Ejercicio7Final.java
@@ -0,0 +1,12 @@
+package poo.cap4ejercicio7final;
+
+public class Cap4Ejercicio7Final {
+
+    public static void main(String[] args) {
+        // Creamos nuevos numeros como objetos
+        
+        VentanaEjercicio7final ventana = new VentanaEjercicio7final();
+        ventana.show(true);
+        
+    }
+}
