74fb314afc84a87f9bf5e1695d604e859b598db9
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-08-18 20:47:14 -0500

    Add files via upload

---
+++ Actividad 4/Animales/Lobo.java
@@ -0,0 +1,19 @@
+package Animales;
+
+public class Lobo extends Canido {
+    public String getSonido() {
+        return "Aullido";
+    }
+
+    public String getAlimentos() {
+        return "Carnivoro";
+    }
+
+    public String getHabitat() {
+        return "Bosque";
+    }
+
+    public String getNombreCientifico() {
+        return "Canis lupus";
+    }
+}
