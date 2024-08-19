c268c8febb13436bf51fef0f7b17ec267cc2c430
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-08-18 20:49:49 -0500

    Add files via upload

---
+++ Actividad 4/CarreraCiclistica/Contrarrelojista.java
@@ -0,0 +1,27 @@
+package CarreraCiclistica;
+
+public class Contrarrelojista extends Ciclista {
+    private double velocidadMaxima;
+
+    public Contrarrelojista(int identificador, String nombre, double velocidadMaxima) {
+        super(identificador, nombre);
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    protected double getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    protected void setVelocidadMaxima(double velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    protected void imprimir() {
+        super.imprimir();
+        System.out.println("Velocidad Maxima: " + getVelocidadMaxima());
+    }
+
+    protected String imprimirTipo() {
+        return "Es un contrarrelojista";
+    }
+}
